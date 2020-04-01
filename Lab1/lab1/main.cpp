faffae280eed2e56ae86ff3855f945edd9562c16
Kanstancin Novikaŭ <kickoman@yandex.by>
2020-04-01 19:50:18 +0300

    Lab 1: Menu improved

--- Lab1/lab1/main.cpp
+++ Lab1/lab1/main.cpp
@@ -27,7 +27,55 @@ struct Student
     }
 };
 
-int safeReadInt(bool* ok = nullptr)
+int safeReadInt(bool *ok = nullptr);
+int getChoice(int minimal, int maximal);
+
+// Menu choice functions
+int getMainMenuChoice();
+
+// Main menu choices
+void createNewRecord(); // 1 - create new record
+void showAllRecords();  // 2 - show all records
+void correctRecord();   // 3 - correct record
+void individualTask();  // 4 - individual task
+void loadFile();        // 5 - load data from file
+void saveToFile();      // 6 - save data to file
+void goodbyeMessage();  // 7 - Quit
+
+// Temp debug functions
+void unimplemented();
+
+int main()
+{
+    bool stopRequested = false;
+    while (!stopRequested)
+    {
+        switch (getMainMenuChoice())
+        {
+        case 1: createNewRecord(); break;
+        case 2: showAllRecords(); break;
+        case 3: correctRecord(); break;
+        case 4: individualTask(); break;
+        case 5: loadFile(); break;
+        case 6: saveToFile(); break;
+        case 7:
+            stopRequested = true;
+            goodbyeMessage();
+            break;
+        default: printf("Undefined behavior. Something wrong happened.\n");
+        }
+
+        getc(stdin);
+    }
+    return 0;
+}
+
+void unimplemented()
+{
+    printf("Sorry, this function is not implemented yet!\n");
+}
+
+int safeReadInt(bool* ok)
 {
     static const unsigned int LINE_SIZE = 255;
     char lineBuffer[LINE_SIZE];
@@ -65,12 +113,18 @@ int getChoice(int minimal, int maximal)
 int getMainMenuChoice()
 {
     int choice = 0;
+    int point = 0;
 
     printf("*** MAIN MENU ***\n\n");
     printf("Number | Action\n");
     printf("---------------\n");
-    printf("%6d | Hello!\n", 1);
-    printf("%6d | World!\n", 2);
+    printf("%6d | Create new record\n", ++point);
+    printf("%6d | Show all records\n", ++point);
+    printf("%6d | Correct record\n", ++point);
+    printf("%6d | Individual task\n", ++point);
+    printf("%6d | Load file\n", ++point);
+    printf("%6d | Save to file\n", ++point);
+    printf("%6d | Quit\n", ++point);
 
     bool ok = false;
 
@@ -85,7 +139,7 @@ int getMainMenuChoice()
             continue;
         }
 
-        if (choice < 1 || choice > 2)
+        if (choice < 1 || choice > point)
         {
             ok = false;
             printf("[ERROR] No such index in menu. Please, correct your input.\n");
@@ -94,8 +148,38 @@ int getMainMenuChoice()
     return choice;
 }
 
-int main()
+void createNewRecord()
 {
-    getMainMenuChoice();
-    return 0;
+    unimplemented();
+}
+
+void showAllRecords()
+{
+    unimplemented();
+}
+
+void correctRecord()
+{
+    unimplemented();
+}
+
+void individualTask()
+{
+    unimplemented();
+}
+
+void loadFile()
+{
+    unimplemented();
+}
+
+void saveToFile()
+{
+    unimplemented();
+}
+
+void goodbyeMessage()
+{
+    printf("Goodbye! Have a nice day!\n");
+    printf("Press Enter key to continue.\n");
 }
