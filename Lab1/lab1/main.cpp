64eb172247f2cbf72eb8761bdf9bf5a7b72d1fc7
BSUIR-LABS Kanstancin <bsuir-labs@yandex.by>
2020-04-08 00:41:49 +0300

    ref #1 Functions for working with files implemented

--- Lab1/lab1/main.cpp
+++ Lab1/lab1/main.cpp
@@ -2,18 +2,40 @@
 #include <cstring>
 #include <climits>
 #include <assert.h>
+#include <stdio.h>
+#include <sys/io.h>     // TODO: pay attention! This is for *nix only
 
 using namespace std;
 
 const unsigned int SURNAME_LEN = 50;
 const unsigned int INITIALS_LEN = 3;
 
+const char DATABASE_FILENAME[] = "students.dat";
+
 // Struct of student and functions for work with this structure
-struct Student;
+struct Student
+{
+    char surname[SURNAME_LEN];
+    char initials[INITIALS_LEN];   // format as in Microsoft Office products (for example, Kanstancin Dzmitryjevic Novikau => Novikau KD)
+
+    unsigned short mark_physics;
+    unsigned short mark_maths;
+    unsigned short mark_informatics;
+    unsigned short mark_chemistry;
+
+    double average_mark;
+
+    static void updateAverage(Student& student)
+    {
+        double sum = student.mark_maths + student.mark_physics + student.mark_chemistry + student.mark_informatics;
+        student.average_mark = sum / 4;
+    }
+};
 void readStudent(Student &student);
 void printStudent(Student student);
 
 int safeReadInt(bool *ok = nullptr);
+void clearInputBuffer();
 int getChoice(int minimal, int maximal);
 
 // Menu choice functions
@@ -28,12 +50,21 @@ void loadFile();        // 5 - load data from file
 void saveToFile();      // 6 - save data to file
 void goodbyeMessage();  // 7 - Quit
 
+// Functions for files
+void createFile(const char filename[]);
+void removeFile(const char filename[]);
+void appendToFile(const char filename[], const char* data, unsigned size);
+void writeToFile(const char filename[], const char* data, unsigned size);
+unsigned readFile(const char filename[], char** buffer, unsigned maxSize = 0);
+unsigned filelength(FILE* file);
+
 // Temp debug functions
 void unimplemented();
 
 int main()
 {
     bool stopRequested = false;
+
     while (!stopRequested)
     {
         switch (getMainMenuChoice())
@@ -61,6 +92,12 @@ void unimplemented()
     printf("Sorry, this function is not implemented yet!\n");
 }
 
+void clearInputBuffer()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
+
 int safeReadInt(bool* ok)
 {
     static const unsigned int LINE_SIZE = 255;
@@ -71,6 +108,7 @@ int safeReadInt(bool* ok)
 
     // get the whole line
     fgets(lineBuffer, LINE_SIZE, stdin);
+    //clearInputBuffer();
     int lineSize = strlen(lineBuffer);
 
     // parse the buffer
@@ -136,12 +174,24 @@ int getMainMenuChoice()
 
 void createNewRecord()
 {
-    unimplemented();
+    printf("*** Creating new record ***\n");
+
+    Student record;
+    readStudent(record);
+
+    appendToFile(DATABASE_FILENAME, (char*)&record, sizeof(record));
 }
 
 void showAllRecords()
 {
-    unimplemented();
+    Student* students = nullptr;
+    unsigned bytes = readFile(DATABASE_FILENAME, (char**)&students);
+    unsigned size = bytes / sizeof(Student);
+
+    for (unsigned i = 0; i < size; ++i)
+        printStudent(students[i]);
+
+    delete[] students;
 }
 
 void correctRecord()
@@ -170,31 +220,18 @@ void goodbyeMessage()
     printf("Press Enter key to continue.\n");
 }
 
-struct Student
-{
-    char surname[SURNAME_LEN];
-    char initials[INITIALS_LEN];   // format as in Microsoft Office products (for example, Kanstancin Dzmitryjevic Novikau => Novikau KD)
-
-    unsigned short mark_physics;
-    unsigned short mark_maths;
-    unsigned short mark_informatics;
-    unsigned short mark_chemistry;
-
-    double average_mark;
-
-    static void updateAverage(Student& student)
-    {
-        double sum = student.mark_maths + student.mark_physics + student.mark_chemistry + student.mark_informatics;
-        student.average_mark = sum / 4;
-    }
-};
-
 void readStudent(Student &student)                      // TODO: refactor
 {
     printf("*** reading student info ***\n\n");
 
-    printf("Surname: "); scanf("%s", student.surname);  // TODO: maybe change to safe scanfs
-    printf("Initials: "); scanf("%s", student.initials);
+    printf("Surname: ");
+    fgets(student.surname, SURNAME_LEN, stdin);
+//    scanf("%s", student.surname);  // TODO: maybe change to safe scanfs
+    //clearInputBuffer();
+    printf("Initials: ");
+    fgets(student.initials, INITIALS_LEN, stdin);
+//    scanf("%s", student.initials);
+    //clearInputBuffer();
 
     bool inner_ok = false;
     while (!inner_ok)                   // TODO: maybe move to a separate function???
@@ -241,4 +278,82 @@ void printStudent(Student student)
     printf("\tChemistry:   %d\n", student.mark_chemistry);
     printf("\tInformatics: %d\n\n", student.mark_informatics);
     printf("\tAverage:     %f\n", student.average_mark);
+}
+
+void createFile(const char filename[])
+{
+    FILE* file = fopen(filename, "wb");
+    if (file == nullptr)
+        fprintf(stderr, "Can't create file %s. Error.\n", filename);
+    else
+    {
+        fclose(file);
+        printf("File %s has been created.\n", filename);
+    }
+}
+
+void removeFile(const char filename[])
+{
+    if (remove(filename) != 0)
+        fprintf(stderr, "Can't remove file %s. Error\n", filename);
+    else
+        printf("Removed file successfully!\n");
+}
+
+void appendToFile(const char filename[], const char* data, unsigned size)   // TODO: refactor (look at writeToFile)
+{
+    FILE* file = fopen(filename, "ab");
+    if (file)
+    {
+        fwrite(data, 1, size, file);
+        fclose(file);
+        printf("%d bytes appended to file %s.\n", size, filename);
+    }
+    else
+    {
+        fprintf(stderr, "Can't write to file %s!\n", filename);
+    }
+}
+
+void writeToFile(const char filename[], const char* data, unsigned size)
+{
+    FILE* file = fopen(filename, "wb");
+    if (file)
+    {
+        fwrite(data, 1, size, file);
+        fclose(file);
+        printf("%d bytes appended to file %s.\n", size, filename);
+    }
+    else
+    {
+        fprintf(stderr, "Can't write to file %s!\n", filename);
+    }
+}
+
+unsigned readFile(const char filename[], char** buffer, unsigned maxSize)
+{
+    FILE* file = fopen(filename, "rb");
+    unsigned read = 0;
+    if (file)
+    {
+        unsigned length = filelength(file);
+        if (maxSize) length = max(length, maxSize);
+        *buffer = new char[length];
+        read = fread(*buffer, 1, length, file);
+        if (ferror(file) != 0)
+            fprintf(stderr, "Some error occurred while reading");
+        fclose(file);
+    }
+    else
+        fprintf(stderr, "Can't open file %s for reading\n", filename);
+    return read;
+}
+
+unsigned filelength(FILE* file)
+{
+    unsigned position = ftell(file);
+    fseek(file, 0, SEEK_END);
+    unsigned length = ftell(file);
+    fseek(file, position, SEEK_SET);
+    return length;
 }
