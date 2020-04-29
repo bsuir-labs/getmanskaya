e26fb07d5f00821396bce1be3b6544c75c4f94a3
BSUIR-LABS Kanstancin <bsuir-labs@yandex.by>
2020-04-30 01:15:16 +0300

    ref #14 Comments for lab 8

--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -1,10 +1,20 @@
 #include <math.h>
 #include <stdio.h>
 
+/// \brief Функция для поиска корней по методу Ньютона
+/// \param a - левая граница диапазона
+/// \param b - правая граница диапазона
+/// \param eps - Максимальная допустимая погрешность
+/// \return значение х, при котором функция равна 0
 double Newton(double a, double b, double eps);
+
+/// \brief Функция x^2 - 10 * sin(x)^2 + 2
 double f(double x);
+
+/// \brief Производная функции f
 double df(double x);
 
+/// безопасный ввод, как в прошлых лабах
 double safeReadDouble(const char *prompt);
 
 int main()
@@ -13,26 +23,27 @@ int main()
     printf("Function: x^2-10*sin(x)^2+2\n");
     printf("Newton method.\n\n");
 
-    double a = safeReadDouble("Specify range start value: ");
+    double a = safeReadDouble("Specify range start value: ");   // получаем значения
     double b = safeReadDouble("Specify range end value: ");
     double eps = safeReadDouble("Specify epsilon: ");
     double h = safeReadDouble("Specify step: ");
 
-    int rootcnt = 0;
-    for (double x = a; x <= b; x += h)
+    int rootcnt = 0;                            // количество найденных корней
+    for (double x = a; x <= b; x += h)          // идём по диапазону
     {
-        if (f(x) * f(x + h) < 0)
-        {
-            double root = Newton(x, x + h, eps);
-            printf("Root #%d: %lf\n", ++rootcnt, root);
+        if (f(x) * f(x + h) < 0)                // если между текущим и следующим значением есть 0,
+        {                                       // то где-то тут есть корень функции
+            double root = Newton(x, x + h, eps);    // Найдём его по методу Ньютона
+            printf("Root #%d: %lf\n", ++rootcnt, root); // И выведем
         }
     }
 
-    if (!rootcnt)
+    if (!rootcnt)   // Если ничего не нашли, то сообщим об этом
         printf("No roots found in the range [%lf; %lf]\n", a, b);
     return 0;
 }
 
+// реализация метода Ньютона из методы
 double Newton(double a, double b, double eps)
 {
     double x1 = (a + b) / 2, delta = 0, x2;
