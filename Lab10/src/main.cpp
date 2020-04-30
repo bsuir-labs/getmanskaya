ded2e1be84524dd502fb1f2577128c4af324ab6f
BSUIR LABS Win10 <bsuir-labs@yandex.by>
2020-04-30 15:53:13 +0300

    ref #16 #14 Comment fixed for lab 10

--- Lab10/src/main.cpp
+++ Lab10/src/main.cpp
@@ -33,55 +33,57 @@ int main()
         fin.open("test-in-" + std::to_string(test) + ".txt");
         fout.open("test-out-" + std::to_string(test) + ".txt");
 
-        if (!fin.is_open() || !fout.is_open())
+        if (!fin.is_open() || !fout.is_open())  // Защита от ошибок на случай, если вдруг файл не смогли открыть
         {
             std::cout << "Can't open files for test #" << test << std::endl;
             continue;
         }
 
-        vector<vector<int>> graph;
-        bool broken = false;
+        vector<vector<int>> graph;      // Тут будет хранится граф
+        bool broken = false;            // Это флаг о том, что текущий тестовый файл с ошибкой
+                                        // Нужен, т.к. мы можем узнать про ошибку в процессе чтения файла
+                                        // и если так, то нам придётся пропустить весь тест.
 
         int a, b;
-        fin >> a >> b;
-        while (!fin.eof())
+        fin >> a >> b;                  // Пробуем прочесть первое ребро из файла
+        while (!fin.eof())              // И будем это делать до тех пор, пока не дойдём до конца файла (eof - End of file)
         {
-            if (a < 1 || b < 1)
+            if (a < 1 || b < 1)         // Если ребро неверное, то придётся тест пропустить
             {
-                cout << "Test #" << test << " ";
+                cout << "Test #" << test << " ";    // Сообщаем об этом в консоль
                 cout << "Node number should be a positive number" << endl;
-                broken = true;
-                break;
+                broken = true;      // И поднимаем флаг, что тест ошибочный
+                break;              // И выходим из этого цикла, дальше тест читать смысла нету
             }
 
-            if (graph.size() < max(a, b))
-                graph.resize(max(a, b));
+            if (graph.size() < max(a, b))   // Поправляем размер графа на ходу, т.е. если вдруг нам впервые встретилась
+                graph.resize(max(a, b));    // вершина с номером 9, то мы считаем, что тут как минимум 9 вершин есть
 
             --a, --b;
-            graph[a].push_back(b);
+            graph[a].push_back(b);          // Сохраняем прочитанную информацию
 
-            fin >> a >> b;
+            fin >> a >> b;                  // И пытаемся прочесть следующее ребро
         }
 
-        if (broken)
+        if (broken)                     // Если вдруг наткнулись на ошибку
         {
-            fin.close();
+            fin.close();                // Закрываем файлы
             fout.close();
-            continue;
+            continue;                   // И пропускаем этот тест
         }
 
-        std::string output;
+        std::string output;             // Сюда запишем ответ
 
-        switch (getType(graph)) {
-        case GraphType::Common: output = "common"; break;
-        case GraphType::Functional: output = "functional"; break;
-        case GraphType::CounterFunctional: output = "counter-functional"; break;
+        switch (getType(graph)) {       // Спрашиваем у функции тип графа
+        case GraphType::Common:             output = "common"; break;               // и в зависимости от полученного значения
+        case GraphType::Functional:         output = "functional"; break;           // записываем ответ в переменную
+        case GraphType::CounterFunctional:  output = "counter-functional"; break;
         default: output = "undefined";
         }
 
         fout << output << endl; // Выводим ответ в файл
 
-        fout.close();
+        fout.close();   // закрываем файлы
         fin.close();
     }
     return 0;
@@ -95,9 +97,9 @@ GraphType getType(const vector<vector<int>> &graph)
     for (size_t i = 0; i < graph.size(); ++i)
         functionalNodes += graph[i].size() == 1;
 
-    if (functionalNodes == graph.size() && functionalNodes > 0)
-        return GraphType::Functional;
-    if (functionalNodes == 0)
-        return GraphType::CounterFunctional;
+    if (functionalNodes == graph.size() && functionalNodes > 0) // Если граф не пустой и у него все вершины имеют только 1 исходящее ребро
+        return GraphType::Functional;                           // то и граф у нас функциональный
+    if (functionalNodes == 0)                                   // А если нету ни одной такой вершины
+        return GraphType::CounterFunctional;                    // то контрфункциональный
     return GraphType::Common;               // А иначе, ни тот, ни другой
 }
