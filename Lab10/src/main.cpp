a5b30706066712cbb06d217e0ee87ef06ee1e6e0
BSUIR LABS Win10 <bsuir-labs@yandex.by>
2020-04-30 15:44:02 +0300

    ref #16 fixed graph reading

--- Lab10/src/main.cpp
+++ Lab10/src/main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
@@ -32,37 +33,28 @@ int main()
             continue;
         }
 
-        int nodesCnt, edgesCnt;
-        fin >> nodesCnt >> edgesCnt;
-
-        if (nodesCnt < 1)
-        {
-            std::cout << "Test #" << test << " ";
-            std::cout << "Number of nodes can't be less than 1, but given " << nodesCnt << std::endl;
-            fin.close();
-            fout.close();
-            continue;
-        }
-
-        vector<vector<int>> graph(nodesCnt);
-
+        vector<vector<int>> graph;
         bool broken = false;
 
-        for (int i = 0; i < edgesCnt; ++i)
+        int a, b;
+        fin >> a >> b;
+        while (!fin.eof())
         {
-            int a, b;
-            fin >> a >> b;
-
-            if (a < 1 || a > nodesCnt || b < 1 || b > nodesCnt)
+            if (a < 1 || b < 1)
             {
-                std::cout << "Test #" << test << " ";
-                std::cout << "Node number should be in range from " << 1 << " to " << nodesCnt << std::endl;
+                cout << "Test #" << test << " ";
+                cout << "Node number should be a positive number" << endl;
                 broken = true;
                 break;
             }
 
+            if (graph.size() < max(a, b))
+                graph.resize(max(a, b));
+
             --a, --b;
             graph[a].push_back(b);
+
+            fin >> a >> b;
         }
 
         if (broken)
@@ -96,7 +88,7 @@ GraphType getType(const vector<vector<int>>& graph)
     for (size_t i = 0; i < graph.size(); ++i)
         functionalNodes += graph[i].size() == 1;
 
-    if (functionalNodes == graph.size())
+    if (functionalNodes == graph.size() && functionalNodes > 0)
         return GraphType::Functional;
     if (functionalNodes == 0)
         return GraphType::CounterFunctional;
